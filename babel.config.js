4d0e66e7c28e21e044151e41ecbf10df21205ac3
Ronroeandassociates <3321@holbertonschool.com>
2022-08-18 08:57:59 -0500

    add the givens

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// given
+
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+};
