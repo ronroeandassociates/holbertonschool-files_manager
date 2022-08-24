b5a8da9978b83bd7e5d18a0d38f4011a337ef088
Ronroeandassociates <3321@holbertonschool.com>
2022-08-24 16:45:45 -0500

    lint it do the body no good

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-
-const router = require('./routes/index');
 const express = require('express');
+const router = require('./routes/index');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
