eb4815178aca048e59b70924d9ec3a8e34204d64
Ronroeandassociates <3321@holbertonschool.com>
2022-08-24 16:04:24 -0500

    0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(error)
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, second) {
+    this.client.set(key, value);
+    this.client.expire(key, second);
+  }
+
+  async del(key) {
+    return this.client.del(key);
+  }
+
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
